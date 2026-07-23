51fdf2f1f622be5d596adf9f72ac2559becac23d
hillarymusika-cell <hillarymusika@gmail.com>
2026-07-24 00:35:05 +0300

    catching new errors

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from init import create_app
 
 app = create_app()
+from auth import oauth
+oauth.init_app(app)
 
 if __name__ == "__main__":
     debug_mode = os.environ.get("FLASK_DEBUG", "0") == "1"
